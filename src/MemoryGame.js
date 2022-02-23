b6763dd8cfb1474ca303236d82a42064c5c32364
Fawlkzin <jeffersonrio77@gmail.com>
2022-02-23 08:27:49 -0300

    initial commit

---
+++ src/MemoryGame.js
@@ -0,0 +1,36 @@
+import React,{useEffect, useState} from "react";
+import GameBoard from "./Components/GameBoard";
+import GameOver from "./Components/GameOver";
+import game from "./Game/game";
+
+export default function MemoryGame(){
+
+    const [gameOver, setGameOver] = useState(false)
+    const [cards, setCards] = useState([])
+
+    useEffect(()=>{
+        setCards(game.createCardsFromTechs())
+    },[])
+
+    function restart(){
+        setGameOver(false)
+    }
+
+    function handleFlip(card){
+       game.flipCard(card.id,()=>{
+           /* GameOverCallback */
+        setGameOver(true)
+       },()=>{
+           /* NomatchCallback */
+           setCards([...game.cards])
+       })
+       setCards([...game.cards])
+    }
+
+    return(
+        <div>
+            <GameBoard handleFlip={handleFlip} cards={cards}></GameBoard>
+            <GameOver show={gameOver} handleRestart={restart}></GameOver>
+        </div>
+    )
+}
